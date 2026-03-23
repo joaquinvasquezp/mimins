05d73f328b933e53179007364569d983e3aeb309
Joaquín Vásquez <joaquin.vasp@gmail.com>
2026-03-23 00:26:35 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["better-sqlite3"],
-  serverActions: {
-    allowedOrigins: [
-      "localhost:3001",
-      "localhost:3000",
-      // Agrega aquí la IP de tu máquina en la red local, ej:
-      // "192.168.1.100:3001",
-    ],
-  },
+  allowedDevOrigins: [
+    "192.168.100.10",
+  ]
 };
 
 export default nextConfig;
